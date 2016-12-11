c1cfddf9ef3fa773248b3cadb2acfb052fbb8b77
cdcyang <cdcyang@gmail.com>
2016-12-11 19:27:44 +0000

    Modified exception in File2d

--- src/ChrisYang/file2d.cpp
+++ src/ChrisYang/file2d.cpp
@@ -18,67 +18,54 @@ string File2d::StringToUpper(string strToConvert) {
 }
 
 void File2d::createInstructionVector() {
-
     ifstream openFile(selected_file.c_str());
 
     // Throw exception if the file cannot be open, catch in the main class.
-    if(!openFile.is_open()) {
-        // throw BadFileException();
-        cerr<<"Unable to open file!"<<endl;
-    }
-
-    cout<<"Reading file: "<<selected_file<<endl;
-    string line;
+    if(!openFile.is_open()) 
+        throw BadFileException();
 
     // If the file is open, loop around the file to search for the words: translate, scale, circle, polygon.
+    cout<<"Reading file: "<<selected_file<<endl;
+    string line;
     while(getline(openFile, line)){
-        int strpos = line.find(" ");
-        string first_word = line.substr(0, strpos);
-        first_word = StringToUpper(first_word);
+        try{
+            int strpos = line.find(" ");
+            string first_word = line.substr(0, strpos);
+            first_word = StringToUpper(first_word);
 
-    // Create new instance for corresponding keywords.
-
-        if(first_word == "TRANSLATE"){
-            try{
+            // Create new instance for corresponding keywords.
+            if(first_word == "TRANSLATE"){
                 Translate* next_translation = new Translate;
                 openFile>>*next_translation;
                 instruction_vector.push_back(next_translation); 
-            }catch(const runtime_error& error){
-                cerr<<"Error occured during createing new translate instance."<<endl;
             }
-        }
 
-        else if(first_word == "SCALE"){
-            try{
+            else if(first_word == "SCALE"){
                 Scale* next_scale = new Scale;
                 openFile>>*next_scale;
                 instruction_vector.push_back(next_scale);    
-            }catch(const runtime_error& error){
-                cerr<<"Error occured during createing new scale instance."<<endl;
             }
-        }
 
-        else if(first_word == "CIRCLE"){
-            try{
+            else if(first_word == "CIRCLE"){
                 Circle* next_circle = new Circle;
                 openFile>>*next_circle;
                 instruction_vector.push_back(next_circle);    
-            }catch(const runtime_error& error){
-                cerr<<"Error occured during createing new circle instance."<<endl;
             }
-        }
 
-        else if(first_word == "POLYGON"){
-            try{
+            else if(first_word == "POLYGON"){
                 Polygon* next_polygon = new Polygon;
                 openFile>>*next_polygon;
                 instruction_vector.push_back(next_polygon);    
-            }catch(const runtime_error& error){
-                cerr<<"Error occured during createing new polygon instance."<<endl;
             }
         }
+        // Catch runtime exception and any other unexception exception
+        catch(const runtime_error& error){
+            cerr<<"Runtime exception"<<endl;
+        }
+        catch(,,,){
+            cerr<<"Encouter exception"<<endl;
+        }
     }
-    
     openFile.close();
 }
 
