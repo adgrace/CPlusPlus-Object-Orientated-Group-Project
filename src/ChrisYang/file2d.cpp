b40ff54ca2fa760aa9d4115d31829a65932eb5cb
cdcyang <cdcyang@gmail.com>
2016-12-11 17:21:29 +0000

    Modified instruction vector

--- src/ChrisYang/file2d.cpp
+++ src/ChrisYang/file2d.cpp
@@ -10,6 +10,8 @@ File2d::~File2d() {
     delete [] instruction_vector;
 }
 
+
+// Modify the input string and output the correponding string in upper case.
 string File2d::StringToUpper(string strToConvert) {
     transform(strToConvert.begin(), strToConvert.end(), strToConvert.begin(), ::toupper);
     return strToConvert;
@@ -18,37 +20,42 @@ string File2d::StringToUpper(string strToConvert) {
 void File2d::createInstructionVector() {
 
     ifstream openFile(selected_file.c_str());
-    cout<<selected_file<<endl;
+
+    // Throw exception if the file cannot be open, catch in the main class.
     if(!openFile.is_open()) throw BadFileException();
+
 
     cout<<"Reading file: "<<selected_file<<endl;
     string line;
+
+    // If the file is open, loop around the file to search for the words: translate, scale, circle, polygon.
     while(getline(openFile, line)){
         int strpos = line.find(" ");
         string first_word = line.substr(0, strpos);
         first_word = StringToUpper(first_word);
 
+    // Create new instance for corresponding keywords.
+
         if(first_word == "TRANSLATE"){
             Translate* next_translation = new Translate;
             openFile>>*next_translation;
-            instruction_vector = (vector3*)next_translation; 
+            instruction_vector.push_back(next_translation); 
         }
         else if(first_word == "SCALE"){
             Scale* next_scale = new Scale;
             openFile>>*next_scale;
-            instruction_vector = (vector3*)next_scale;    
+            instruction_vector.push_back(next_scale);
             
         }
         else if(first_word == "CIRCLE"){
             Circle* next_circle = new Circle;
-            openFile>>next_circle;
-            instruction_vector = (vector3*)next_circle;
+            openFile>>*next_circle;
+            instruction_vector.push_back(next_circle);
         }
         else if(first_word == "POLYGON"){
             Polygon* next_polygon = new Polygon;
-            openFile>>next_polygon;
-            instruction_vector = (vector3*)next_polygon;
-
+            openFile>>*next_polygon;
+            instruction_vector.push_back(next_polygon);
         }
     }
     openFile.close();
